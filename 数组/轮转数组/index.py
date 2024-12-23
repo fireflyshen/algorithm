f6a855481ae2cc83a8fbc716f0f34f033b6065c0
fireflyshen <59105078+fireflyshen@users.noreply.github.com>
2024-12-23 23:23:07 +0800

    h

---
+++ 数组/轮转数组/index.py
@@ -0,0 +1,13 @@
+# 189.轮转数组
+
+from typing import List;
+
+class Solution:
+    def _189_rotate(self,nums:List[int],k:int) -> None: 
+        k %= len(nums)
+        nums[:] = nums[-k:] + nums[:-k]
+        print(nums)
+
+
+s = Solution();
+s._189_rotate([1,2,3,4,5,6:q,7],3);
